25a05341219bfc78b2d360420337999684c44275
Michael Owens <michael@insided.nl>
2013-12-05 17:18:33 +0100

    Added font awesome for icons, added a toolbar with buttons, pencil and brush1 are now working, did some refactoring and created some objects to separate logic

--- script.js
+++ script.js
@@ -1,12 +1,71 @@
-(function (window) {
+/*jslint browser: true, plusplus: true */
+var Helpers = {},
+    Draw = {},
+    Toolbar = {};
+
+/**
+ * Helpers
+ */
+(function (helpers) {
+    'use strict';
+    /**
+     * Checks if an element has a class
+     * @source http://rockycode.com/blog/addremove-classes-raw-javascript/
+     * @param el
+     * @param className
+     * @returns {boolean}
+     */
+    helpers.hasClass = function (el, className) {
+        return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+    };
+
+    /**
+     * Adds a class to an element
+     * @param el
+     * @param className
+     * @returns {boolean}
+     */
+    helpers.addClass = function (el, className) {
+        if (this.hasClass(el, className)) {
+            return true;
+        }
+
+        el.className += ' ' + className;
+        return true;
+    };
+
+    /**
+     * Removes a class from an element
+     * @source http://rockycode.com/blog/addremove-classes-raw-javascript/
+     * @param el
+     * @param className
+     * @returns {boolean}
+     */
+    helpers.removeClass = function (el, className) {
+        if (!this.hasClass(el, className)) {
+            return false;
+        }
+
+        el.className = el.className.replace(new RegExp('(\\s|^)' + className + '(\\s|$)'), ' ');
+        return true;
+    };
+}(Helpers));
+
+/**
+ * Draw object
+ */
+(function (draw) {
     'use strict';
 
     var el = document.getElementById('drawing'),
         ctx = el.getContext('2d'),
-        points = [], isDrawing;
+        points = [],
+        isDrawing;
 
-    el.width = document.body.clientWidth;
-    el.height = document.body.clientHeight;
+    draw.init = function () {
+        el.width = document.body.clientWidth - 50;
+        el.height = document.body.clientHeight;
+    };
 
     ctx.lineWidth = 1.2;
     ctx.lineJoin = ctx.lineCap = 'round';
@@ -18,16 +77,24 @@
     };
 
     el.onmousemove = function (e) {
-        if (!isDrawing) return;
+        if (!isDrawing) {
+            return;
+        }
 
         points.push({x: e.clientX, y: e.clientY});
 
         var len = points.length,
             last_point = points[len - 1],
             second_last_point = points[len - 2],
-            current_point, d, dx, dy;
+            current_point,
+            d,
+            dx,
+            dy;
 
         ctx.strokeStyle = 'rgba(255,255,255,0.1)';
+        if (Toolbar.activeTool === 'pencil') {
+            ctx.strokeStyle = 'rgba(255,255,255,0.5)';
+        }
 
         ctx.beginPath();
         ctx.moveTo(second_last_point.x, second_last_point.y);
@@ -42,12 +109,14 @@
 
             d = dx * dx + dy * dy;
 
-            // nearest neighbor
-            if (d < 1000) {
-                ctx.beginPath();
-                ctx.moveTo(last_point.x + (dx * 0.2), last_point.y + (dy * 0.2));
-                ctx.lineTo(current_point.x - (dx * 0.2), current_point.y - (dy * 0.2));
-                ctx.stroke();
+            if (Toolbar.activeTool === 'brush1') {
+                // nearest neighbor
+                if (d < 1000) {
+                    ctx.beginPath();
+                    ctx.moveTo(last_point.x + (dx * 0.2), last_point.y + (dy * 0.2));
+                    ctx.lineTo(current_point.x - (dx * 0.2), current_point.y - (dy * 0.2));
+                    ctx.stroke();
+                }
             }
 
             // nearest neighbor furry
@@ -64,4 +133,45 @@
         isDrawing = false;
         points = [];
     };
-}(this));
+}(Draw));
+
+/**
+ * Toolbar object
+ */
+(function (toolbar) {
+    'use strict';
+
+    var activeButton;
+
+    toolbar.activeTool = 'pencil';
+
+    toolbar.init = function () {
+        this.bindButtons();
+    };
+
+    toolbar.bindButtons = function () {
+        var buttons = document.querySelectorAll('.toolbar .btn'),
+            buttonsIteration = buttons.length,
+            button;
+
+        while (buttonsIteration--) {
+            button = buttons[buttonsIteration];
+            button.addEventListener('click', this.onClick);
+
+            if (Helpers.hasClass(button, 'active')) {
+                activeButton = button;
+                this.activeTool = button.getAttribute('tool');
+            }
+        }
+    };
+
+    toolbar.onClick = function () {
+        Helpers.removeClass(activeButton, 'active');
+        Helpers.addClass(this, 'active');
+        activeButton = this;
+        toolbar.activeTool = this.getAttribute('tool');
+    };
+}(Toolbar));
+
+Draw.init();
+Toolbar.init();
